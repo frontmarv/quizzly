452e988705c7e374474a5920ccc0ea794c3ce081
marv_len <marvin.lenhart@gmx.net>
2026-07-15 23:13:46 +0200

    Add CORS_ALLOW_CREDENTIALS setting to enable credentials in CORS requests

--- core/settings.py
+++ core/settings.py
@@ -69,6 +69,8 @@ CORS_ALLOWED_ORIGINS = [
     origin.strip() for origin in os.getenv('CORS_ALLOWED_ORIGINS', 'http://127.0.0.1:5500,http://localhost:5500').split(',')
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
